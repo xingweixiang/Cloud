7896cec278e8b3b95452d141661096029e67941c
邢维祥 <xingwx@xjj.com>
2019-12-09 21:29:26 +0800

    实现高可用的服务治理(eureka)

---
+++ service-manage/eureka-service/src/main/java/com/cloudnative/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.cloudnative.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by xingwx on 2019/12/7
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
